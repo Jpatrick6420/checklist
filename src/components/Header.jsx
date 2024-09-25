c36d0d8af609f5a4a9f1fc32fb51517d05b2d33b
Justin Patrick <jpatrick6420@gmail.com>
2024-09-25 08:46:31 -0600

    changed list name size and boldness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,62 +2,24 @@
 import { ItemsContext } from "../context/reducerContext";
 import { useContext } from "react";
 import Nav from "./Nav";
+import { capitalizeName } from "../utilities/utilities";
 /*eslint react/prop-types: 0 */
 
 function Header() {
   const { state } = useContext(ItemsContext);
 
   let activity =
-    state.currentActivityValue === "none" ? "" : state.currentActivityValue;
+    state.currentActivityValue === "none"
+      ? ""
+      : capitalizeName(state.currentActivityValue);
   return (
     <header className="bg-gradient-to-br from-blue-400 px-2 sm:px-4 py-6">
-      <h1 className="mb-4 font-semibold text-gray-900 dark:text-stone-200">
-        {state.currentWeatherValue.split("-").join(" ")}{" "}
-        {activity === "" ? "" : "&"} {activity} list{activity === "" ? "" : "s"}
+      <h1 className="mb-4 font-bold text-gray-900 dark:text-stone-200 text-4xl md:text-5xl">
+        {capitalizeName(state.currentWeatherValue.split("-").join(" "))}{" "}
+        {activity === "" ? "" : "&"} {activity} List
+        {activity === "" ? "" : "s"}
       </h1>
 
-      {/* <div className="input-container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
-        <Select
-          onChange={handleWeatherChange}
-          options={["hot-months", "cold-months", "shoulder-season"]}
-          id="weather-change-input"
-          label="Weather"
-          value={state.currentWeatherValue}
-          dispatch={dispatch}
-        />
-        <Select
-          onChange={handleActivityChange}
-          options={["none", "biking", "kayaking"]}
-          id="activity-change-input"
-          label="activity"
-          value={state.currentActivityValue}
-          dispatch={dispatch}
-        />
-        <Select
-          onChange={handleNumOfPeopleChange}
-          options={["1", "2", "3", "4", "5", "6"]}
-          id="number-of-people-input"
-          label="Number of People"
-          value={state.current}
-          dispatch={dispatch}
-        />
-        <Select
-          onChange={handleDurationChange}
-          options={["1", "2", "3", "4", "5", "6", "7", "8", "9"]}
-          id="number-of-nights-input"
-          label="Number of Nights"
-          value={state.duration}
-          dispatch={dispatch}
-        />
-        <div className="items-center flex">
-          <Link
-            to="/new-entry"
-            className="w-6 h-6 ml-2 bg-blue-500 rounded-md text-stone-50 hover:scale-105 active:scale-100 hover:bg-blue-400 hover:text-stone-50"
-          >
-            +
-          </Link>
-        </div>
-      </div> */}
       <Nav />
     </header>
   );
